0f8bc329f804fc4cdab62cf7b5b2b164f3027de4
Nadav <nadav.freedman22@gmail.com>
2022-04-01 18:16:46 +0300

    Re add project

---
+++ AutomatonBuilder/Interfaces/IAction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomatonBuilder.Entities
+{
+    public interface IAction
+    {
+        public void DoAction();
+
+        public void UndoAction();
+    }
+}
