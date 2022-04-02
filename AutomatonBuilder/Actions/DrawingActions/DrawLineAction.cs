6040b759e34bcf8c1430a8d01f17145da08d822f
Nadav <nadav.freedman22@gmail.com>
2022-04-02 10:17:38 +0300

    Implement the draw line action

--- AutomatonBuilder/Actions/DrawingActions/DrawLineAction.cs
+++ AutomatonBuilder/Actions/DrawingActions/DrawLineAction.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Shapes;
 
 namespace AutomatonBuilder.Entities.Actions
 {
-    public class DrawLineAction
+    public class DrawLineAction : IAction
     {
+        private readonly List<Ellipse> drawnLine;
+        private readonly AutomatonContext context;
+
+        public DrawLineAction(AutomatonContext context)
+        {
+            this.context = context;
+            this.drawnLine = context.CurrentLine!;
+        }
+        public void DoAction()
+        {
+            return;
+        }
+
+        public void RedoAction()
+        {
+            foreach (Ellipse dotToAdd in this.drawnLine)
+                context.MainCanvas.Children.Add(dotToAdd);
+        }
+
+        public void UndoAction()
+        {
+            foreach (Ellipse dotToRemove in this.drawnLine)
+                context.MainCanvas.Children.Remove(dotToRemove);
+        }
     }
 }
